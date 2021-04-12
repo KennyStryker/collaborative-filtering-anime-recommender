a25b36e84d3d92565f3dd2229f04f940d9519336
Kenny <nggkenny@gmail.com>
2021-04-12 11:16:46 +0530

    Commit

--- preprocessing.py
+++ preprocessing.py
@@ -2,5 +2,6 @@ import pandas as pd
 import numpy as np
 
 anime_raw_data = pd.read_csv("dataset\\raw\\anime.csv")
+rating_raw_data = pd.read_csv("dataset\\raw\\rating.csv")
 
 print(anime_raw_data.head())
